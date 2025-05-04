66babc20db2ee43de52002c265a7caaff6a41685
chocozzang <jangjh_05@naver.com>
2025-05-04 18:21:49 +0900

    Movie Detail

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,23 +1,35 @@
-import { Metadata } from "next"
-import { API_URL } from "../../../(home)/page"
+// import { Metadata } from "next"
+// import { API_URL } from "../../../(home)/page"
 import MovieVideos from "../../../../components/movie-videos"
-import MovieInfo from "../../../../components/movie-info"
+import MovieInfo, { getMovie } from "../../../../components/movie-info"
 import { Suspense } from "react"
 
-export const metadata : Metadata = {
-    title : "Movie Detail",
+interface IParams {
+    params : {id : string},
 }
 
-export default async function MovieDetail(
-    {params : {id}, } : {params : {id : string}}) {
+export async function generateMetadata({params : {id}} : IParams) {
+    const movie = await getMovie(id);
+
+    return {
+        title : movie.title,
+    }
+}
+
+// export const metadata : Metadata = {
+//     title : "Movie Detail",
+// }
+
+export default async function MovieDetailPage(
+    {params : {id}} : IParams) {
 
     return (
         <div>
-            <h3>Movie Detail Page</h3>
+            {/* <h3>Movie Detail Page</h3> */}
             <Suspense fallback={<h1>Loading movie info</h1>}>
                 <MovieInfo id={id} />
             </Suspense>
-            <h3>Videos</h3>
+            {/* <h3>Videos</h3> */}
             <Suspense fallback={<h1>Loading movie video</h1>}>
                 <MovieVideos id={id} />
             </Suspense>
